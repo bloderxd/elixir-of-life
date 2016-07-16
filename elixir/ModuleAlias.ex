a652f8e983cfc6ea00f2aa451da5d799c728379b
Daniel <wowmegadenis@hotmail.com>
2016-07-16 16:18:47 -0300

    Change wrong alias type

--- elixir/ModuleAlias.ex
+++ elixir/ModuleAlias.ex
@@ -1,7 +1,7 @@
 defmodule ModuleAlias do
 
-  def alias_sum x,y do
-    alias OperationsAlias, as: Op
-    Op.sum(x,y)
+  def enum_alias x do
+    alias Enum, as: En
+    En.map x, fn x -> x + 2 end
   end
 end
