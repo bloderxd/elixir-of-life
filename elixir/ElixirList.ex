001093ef4fb68953ded73c44ab5a64bdccc5e2e0
Daniel <wowmegadenis@hotmail.com>
2016-07-16 19:23:06 -0300

    Add sum collection function to elixir list module

--- elixir/ElixirList.ex
+++ elixir/ElixirList.ex
@@ -4,4 +4,8 @@ defmodule ElixirList do
   def lenght [_head | tail] do
     1 + length tail
   end
+
+  def sum_list(collection), do: _sum(collection, 0)
+  defp _sum([], total), do: total
+  defp _sum([head | tail], total), do: _sum(tail, head + total)
 end
