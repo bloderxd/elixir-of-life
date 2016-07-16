672baf048825d7475befa15f4d1608e7f838199b
Daniel <wowmegadenis@hotmail.com>
2016-07-16 14:04:27 -0300

    Add other operations to operations module

--- OperationsAlias.ex
+++ OperationsAlias.ex
@@ -3,4 +3,16 @@ defmodule OperationsAlias do
   def sum x,y do
     x + y
   end
+
+  def sub x,y do
+    x - y
+  end
+
+  def mult x,y do
+    x * y
+  end
+
+  def div x,y do
+    x / y
+  end
 end
