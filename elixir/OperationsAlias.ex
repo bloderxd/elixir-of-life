20b34fa38f3da7d4bb85b56e336281098e7344be
Daniel <wowmegadenis@hotmail.com>
2016-07-30 01:41:28 -0300

    Add for example function

--- elixir/OperationsAlias.ex
+++ elixir/OperationsAlias.ex
@@ -15,4 +15,12 @@ defmodule OperationsAlias do
   def div x,y do
     x / y
   end
+
+  defp people do
+    people = [%{name: "Daniel", age: 15}, %{name: "Bloder", age: 18}, %{name: "Z1k4", age: 22}]
+  end
+
+  def for_func do
+    for person = %{age: age} <- people, age > 16, do: person
+  end
 end
