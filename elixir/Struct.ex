c67df40e5704c0a5c18010803204e9785d93dd0a
Daniel <wowmegadenis@hotmail.com>
2016-07-31 01:16:53 -0300

    Add user sign up action example

--- elixir/Struct.ex
+++ elixir/Struct.ex
@@ -1,3 +1,19 @@
 defmodule Struct do
-  defstruct name: "", age: 0
+  defstruct name: "Not Found", age: 0, paid: false
+
+  def show_user(user = %Struct{}) do
+    IO.puts "name: #{user.name} age: #{user.age}"
+  end
+
+  def sign_up_user(%Struct{name: name, age: age, paid: paid}) when age < 18 do
+    IO.puts "You don't have the required age"
+  end
+
+  def sign_up_user(%Struct{name: name, age: age, paid: paid}) when paid == false do
+    IO.puts "You need to buy the sign up"
+  end
+
+  def sign_up_user(%Struct{name: name, age: age, paid: paid}) do
+    IO.puts "Thank you to subscribe!"
+  end
 end
